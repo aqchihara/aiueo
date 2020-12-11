806b451d560a7b198b90bf77587c5e408e0b8593
Takeru Chihara <>
2020-12-11 11:30:32 +0900

    pwa化

---
+++ public/serviceWorker.js
@@ -0,0 +1,27 @@
+var CACHE_NAME  = "MKit-cache-v2";
+var urlsToCache = [
+    "https://aiueo-learn.web.app/",
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(
+            function(cache){
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(
+        function (response) {
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
+        })
+    );
+});
