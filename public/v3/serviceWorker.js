916ad3eeb644f2a775c9cd1e4f52b68b640bb1c0
Takeru Chihara <>
2020-12-28 04:19:14 +0900

    全画面表示

---
+++ public/v3/serviceWorker.js
@@ -0,0 +1,27 @@
+var CACHE_NAME = "Aiueo-cache-v3-1";
+var urlsToCache = [
+    "https://aiueo-learn.web.app/v3",
+];
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(
+                function (cache) {
+                    return cache.addAll(urlsToCache);
+                })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(
+                function (response) {
+                    if (response) {
+                        return response;
+                    }
+                    return fetch(event.request);
+                })
+    );
+});
