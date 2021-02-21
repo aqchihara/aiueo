de1774ab0f4554318f920c9d0ded77c584e3bc2e
Takeru Chihara <>
2021-02-21 23:15:22 +0900

    v14対応

--- public/v14/serviceWorker.js
+++ public/v14/serviceWorker.js
@@ -1,27 +1,27 @@
-var CACHE_NAME = "Aiueo-cache-v13-1";
+var CACHE_NAME = "Aiueo-cache-v14-1";
 var urlsToCache = [
-    // "https://aiueo-learn.web.app/v13/",
+    // "https://aiueo-learn.web.app/v14/",
 ];
 
-self.addEventListener('install', function (event) {
+self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(
-                function (cache) {
-                    return cache.addAll(urlsToCache);
-                })
+        .then(
+            function(cache) {
+                return cache.addAll(urlsToCache);
+            })
     );
 });
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', function(event) {
     event.respondWith(
         caches.match(event.request)
-            .then(
-                function (response) {
-                    if (response) {
-                        return response;
-                    }
-                    return fetch(event.request);
-                })
+        .then(
+            function(response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
     );
 });
